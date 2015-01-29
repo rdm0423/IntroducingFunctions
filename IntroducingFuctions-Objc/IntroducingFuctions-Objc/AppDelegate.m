8beded6e95ca0ca9b3728f5f90aa351463353ead
Ross McIlwaine <rdm0423@gmail.com>
2015-01-29 16:15:25 -0700

    First attempt at IntroductingFunctions
    I am getting errors of numberOfDays not being defined and not sure where I am making the mistake of not defning, I thought I defined in NSString

--- IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
+++ IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
@@ -19,6 +19,29 @@
 
     // Insert code here...
     
+    NSString *name = @"Ross McIlwaine";
+  
+    NSString *printIntroductions(NSString *name);
+    {
+    NSString *introduction = [NSString stringWithFormat:@"My name is %@", name];
+    NSString *japaneseIntro = [name stringByAppendingString:@" to moushimasu"];
+    
+    NSLog(@"%@", introduction);
+    NSLog(@"%@", japaneseIntro);
+    }
+    
+    NSString *countDownTillIntroduction(NSString *numberOfDays);
+    {
+    if (numberOfDays == 0)
+    {
+        NSLog(@"The time has come");
+    }
+    else {
+        NSLog(@"%ld days left until introductions", numberOfDays);
+        NSString *oneLessDay = numberOfDays -1;
+        countDownTillIntroduction(oneLessDay);
+    }
+    }
     
     return YES;
 }
